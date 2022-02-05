b8ceae8b4386a7d1031c93a24d640be3de7db7c7
tayek333 <tarekbukhari123@gmail.com>
2022-02-05 18:31:37 -0500

    refactoring hero and header

---
+++ src/components/Section1.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Section = styled.section`
+display: flex;
+padding: 6vw 5% 0px;
+background-color: rgb(255, 255, 255);
+height: 100vh;
+}
+`
+
+function Section1() {
+    return(
+        <Section>
+            
+        </Section>
+    )
+}
+
+export default Section1
