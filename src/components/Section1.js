628a2366d93927c54b892788f51f19813c95d7b6
tayek333 <tarekbukhari123@gmail.com>
2022-02-20 18:44:23 -0500

    style: section 1 mobile responsive

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -6,12 +6,18 @@ import gas from '../images/gas.svg';
 import hp from '../images/hp.svg';
 import mph from '../images/mph.svg';
 import RangeRover from '../images/2018-Range-Rover-Velgar.png';
+import { media } from './styles/global.styles'
 
 const Section = styled.section`
 display: flex;
 padding: 6vw 5% 0px;
 background-color: rgb(255, 255, 255);
 height: 100vh;
+
+${media.m} {
+    flex-direction: column-reverse;
+    margin-bottom: 10vw;
+    height: unset;
 }
 `
 const Card = styled.div`
@@ -22,6 +28,19 @@ height: 70vh;
 width: 33vw;
 padding: 30px;
 min-height: 40vw;
+
+${media.m} {
+    margin-top: 1vh;
+    height: 60vh;
+    width: 100%;
+    padding: 30px;
+    min-height: 70vw;
+}
+
+${media.s} {
+    height: 45vh;
+    min-height: 80vw;
+}
 `
 const BackgroundContainer = styled.div`
 position: absolute;
@@ -78,16 +97,35 @@ top: 0px;
 left: 0px;
 width: 100%;
 height: 100%;
+
+${media.m} {
+    top: 13vw;
+    left: 5vw;
+}
 `
 const ProductTitle = styled.div`
 margin-top: 25%;
 margin-left 20%;
+
+${media.m} {
+    width: 66%;
+    margin-top: 5%;
+    margin-left: 0%;
+}
+${media.s} {
+    margin-top: -*;
+}
 `
 const H3 = styled.h3`
 color: black;
 font-size: 4vw;
 font-weight: normal;
 margin: 0px;
+
+${media.m} {
+    font-size: 9vw;
+    margin: 0px;
+}
 `
 const H4 = styled.h4`
 color: #4B5168;
@@ -95,22 +133,49 @@ font-size: 1.08vw;
 font-weight: normal;
 margin: 0px;
 line-height: 0.5;
+
+${media.m} {
+    font-size: 3.5vw;
+    line-height: 0.8;
+}
 `
 const Table = styled.table`
 width: 42%;
 margin-left: 20%;
 margin-top: 13%;
+
+${media.m} {
+    width: 55%;
+    margin-left: 0%;
+    margin-top: 5%;
+}
 `
 const Row = styled.tr`
 height: 3vw;
+
+${media.m} {
+    height: 12vw;
+}
 `
 const Icon = styled.img`
 width: 1.45vw;
+
+${media.m} {
+    width: 6vw;
+}
 `
 const Span = styled.span`
 position: relative;
 top: -0.3vw;
 left: 0.6vw;
+font-size: 1vw;
+
+${media.m} {
+    position: relative;
+    top: -1.3vw;
+    left: 2vw;
+    font-size: 4vw
+}
 `
 const Specs = styled.td`
 color: rgb(0, 0, 0);
@@ -118,18 +183,31 @@ font-family: "Bebas Neue", sans-serif;
 font-size: 2.2vw;
 font-weight: normal;
 padding-top: 5px;
+
+${media.m} {
+    font-size: 7vw;
+    padding-top: 0.5vw
+}
 `
 const IconContainer = styled.td`
 color: rgb(0, 0, 0);
 font-family: "Bebas Neue", sans-serif;
 font-size: 1vw;
 font-weight: normal;
+
+${media.m} {
+    font-size: 3.5vw;
+}
 `
 const CarContainer = styled.div`
 position: absolute;
 width: 43vw;
-bottom: -11vh;
+bottom: -9.5vh;
 left: 5.6vw;
+
+${media.m} {
+    width: 90vw;
+}
 `
 const CarImage = styled.img`
 width: 100%;
@@ -138,6 +216,10 @@ const HeadingContainer = styled.div`
 flex: 1 1 0%;
 padding-left: 10vw;
 margin-top: 10vh;
+
+${media.m} {
+    padding-left: 0px;
+    margin-top: 18vh;
 }
 `
 const H2 = styled.h2`
@@ -147,16 +229,31 @@ font-weight: normal;
 line-height: 6vw;
 margin: 0px;
 white-space: pre-line;
+
+${media.m} {
+    font-size: 11vw;
+    line-height: 11vw;
+    white-space: unset;
+}
 `
 const P = styled.p`
 color: rgb(0, 0, 0);
 font-size: 1vw;
 font-weight: normal;
 line-height: 1.8vw;
-width: 90%;
+width: 60%;
 white-space: pre-line;
 margin-top: 2vw;
 font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif;
+
+${media.m} {
+    margin-top: 4vw;
+    font-size: 3.5vw;
+    line-height: 6vw;
+    white-space: unset;
+    width: 100%;
+
+}
 `
 function Section1() {
     return(
@@ -219,10 +316,8 @@ function Section1() {
                     Select a Vehicle From Your Phone.
                 </H2>
                 <P>
-                    Select from a wide range of luxury vehicles in our inventory.
-                    <br />   
+                    Select from a wide range of luxury vehicles in our inventory. 
                     Drive it for a month, trade it the next for something else you have 
-                    <br />
                     always wanted to own.
                 </P>
             </HeadingContainer>
