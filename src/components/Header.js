efaf060c5c0d83a0c203aed5f3434b1c61dd95ec
tayek333 <tarekbukhari123@gmail.com>
2022-02-20 18:43:43 -0500

    style: header mobile responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from 'styled-components';
 import logo from '../images/logo.png';
+import { media } from './styles/global.styles'
 
 const Logo = styled.img`
 height: 100%;
@@ -9,23 +10,27 @@ margin-left: 5px;
 const HeaderContainer = styled.div`
 display: flex;
 height: 100px;
+width: 100%;
 align-items: center;
 padding: 0px 5%;
 position: absolute;
 top: 0px;
 left: 0px;
-background: #FBC843;
 `
-const LogoContainer = styled.div `
-
+const LogoLink = styled.a`
+z-index: 1;
+height: 1vw;
+${media.m} {
+    height: 3.882vw;
+}
 `
 
 function Header() {
     return(
         <HeaderContainer>
-            <LogoContainer>
+            <LogoLink>
                 <Logo src={logo}/>
-            </LogoContainer>
+            </LogoLink>
         </HeaderContainer>
     )
 }
