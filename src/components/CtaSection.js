a128a49c51362c30c3205bc9e014b6c20d820a82
tayek333 <tarekbukhari123@gmail.com>
2022-02-20 18:45:09 -0500

    style: ctaSection mobile responsive

--- src/components/CtaSection.js
+++ src/components/CtaSection.js
@@ -3,12 +3,31 @@ import styled from 'styled-components'
 import Guy from '../images/guy.png'
 import Car from '../images/car2.png'
 import Arrowsvg from '../images/arrow.svg'
+import { media } from './styles/global.styles'
 
 const Section = styled.section`
 display: flex;
 padding: 6vw 5% 0px;
 background-color: rgb(255, 255, 255);
 height: 100vh;
+
+a {
+    color: rgb(65, 74, 105);
+    font-size: 16px;
+    font-weight: bold;
+    letter-spacing: -0.47px;
+    line-height: 54px;
+    text-decoration: none;
+    margin-left: 5px;
+    font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif;
+    }
+    
+${media.m} {
+    flex-direction: column-reverse;
+    padding: 0px 5%;
+    height: unset;
+    overflow: hidden;
+}
 `
 const CardContainer = styled.div`
 display: flex;
@@ -18,6 +37,14 @@ align-items: flex-end;
 position: relative;
 height: 80vh;
 width: 40vw;
+
+${media.m} {
+    position relative;
+    height: unset;
+    width: 120%;
+    left: -10%;
+    margin-top: 2vh;
+}
 `
 const CardWrapper = styled.div`
 position: absolute;
@@ -35,6 +62,10 @@ background-color: rgb(251, 200, 67);
 const CenterImg = styled.img`
 width: 70%;
 z-index: 0;
+
+${media.m} {
+    margin-top: 20%;
+}
 `
 const CarImg = styled.img`
 position: absolute;
@@ -47,6 +78,11 @@ const HeadingContainer = styled.div`
 flex: 1 1 0%;
 margin-left: 6vw;
 margin-top: 10vh;
+
+${media.m} {
+    flex: unset;
+    margin-left: 0px;
+}
 `
 const H2 = styled.h2`
 color: rgb(43, 49, 68);
@@ -55,6 +91,12 @@ font-weight: normal;
 line-height: 6vw;
 margin: 0px;
 white-space: pre-line;
+
+${media.m} {
+    font-size: 11vw;
+    line-height: 11vw;
+    white-space: unset;
+}
 `
 const P = styled.p`
 color: rgb(0, 0, 0);
@@ -65,6 +107,14 @@ width: 90%;
 white-space: pre-line;
 margin-top: 2vw;
 font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif;
+
+${media.m} {
+    margin-top: 4vw;
+    font-size: 3.5vw;
+    line-height: 6vw;
+    white-space: unset;
+    width: 100%;
+}
 `
 const P2 = styled.p`
 color: rgb(0, 0, 0);
@@ -73,12 +123,23 @@ font-weight: 600;
 letter-spacing: 0.02vw;
 line-height: 1.6vw;
 font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif;
+
+${media.m} {
+    font-size: 3vw;
+    letter-spacing: 0.02vw;
+    line-height: 3vw;
+}
 `
 const Divider = styled.div`
 background: rgb(244, 244, 245) none repeat scroll 0% 0%;
 height: 0.25vw;
 width: 100%;
 margin: 4.4vh 0px;
+
+${media.m} {
+    height: 1vw;
+    margin: 4.4vh 0px 1vh;
+}
 `
 const CtaComponent = styled.div`
 
