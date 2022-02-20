30f53562959da25fa8124347b261b41f716baf27
tayek333 <tarekbukhari123@gmail.com>
2022-02-20 18:44:33 -0500

    style: section 2 mobile responsive

--- src/components/Section2.js
+++ src/components/Section2.js
@@ -1,18 +1,29 @@
 import React from "react";
 import styled from 'styled-components';
-import MapImg from '../images/map.png'
-import PhoneImg from '../images/phone.png'
-import Porsche from '../images/porsche.png'
+import MapImg from '../images/map.png';
+import PhoneImg from '../images/phone.png';
+import Porsche from '../images/porsche.png';
+import { media } from './styles/global.styles';
 
 const Section = styled.section`
 display: Flex;
 padding: 6vw 5% 0px;
 background-color: white;
 height: 100vh;
+
+${media.m} {
+    height: unset;
+    margin-top: 6vw;
+    flex-direction: column;
+}
 `
 const HeadingContainer = styled.div`
 flex: 1 1 0%;
 margin-top: 10%;
+
+${media.m} {
+    margin-top: 30%;
+}
 `
 const H2 = styled.h2`
 color: rgb(43, 49, 68);
@@ -21,6 +32,14 @@ font-weight: normal;
 line-height: 6vw;
 margin: 0px;
 white-space: pre-line;
+margin-right: 20%;
+
+${media.m} {
+    font-size: 11vw;
+    line-height: 11vw;
+    white-space: unset;
+    margin-right: 0px;
+}
 `
 const P = styled.p`
 color: rgb(0, 0, 0);
@@ -31,12 +50,28 @@ width: 90%;
 white-space: pre-line;
 margin-top: 2vw;
 font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif;
+padding-right: 30%;
+${media.m} {
+    margin-top: 4vw;
+    font-size: 3.5vw;
+    line-height: 6vw;
+    white-space: unset;
+    width: 100%;
+    padding-right: 0%;
+}
 `
 const ImageContent = styled.div`
 position: relative;
 flex: 1.5 1 0%;
 height: 34.9vw;
 box-sizing: border-box;
+
+${media.m} {
+    overflow: hidden;
+    width: 110%;
+    margin-left: -5%;
+    padding: 45vh 0px;
+}
 `
 const MapWrapper = styled.div`
 position: absolute;
@@ -44,6 +79,14 @@ left: 0px;
 width: 50vw;
 margin-top: 8%;
 overflow: hidden;
+
+${media.m} {
+    left: 50%;
+    top: 50%;
+    margin-top: 0px;
+    width: 130%;
+    transform: translate(-57%, -50%)
+}
 `
 const Map = styled.img`
 width: 100%;
@@ -62,7 +105,14 @@ width: 36.7vh;
 font-size: 0px;
 border-radius: 35px;
 filter: drop-shadow(rgba(0, 0, 0, 0.4) -20px 20px 25px);
-
+transform: translate(0%);
+opacity: 1;
+${media.m} {
+    left: 50%;
+    top: 50%;
+    width: 30vh;
+    transform: translate(-50%, -50%);
+}
 `
 const Phone = styled.img`
 width: 100%;
@@ -72,15 +122,11 @@ function Section2() {
         <Section>
             <HeadingContainer>
                 <H2>
-                    Delivered To 
-                    <br />
-                    Your Door.
+                    Delivered To Your Door.
                 </H2>
                 <P>
                     Eleanor works with your schedule to have a 
-                    <br />
                     white-glove delivery service deliver your new
-                    <br />
                     vehicle right to your door.
                 </P>
             </HeadingContainer>
