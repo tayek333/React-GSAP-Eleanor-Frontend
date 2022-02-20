6781ef5b7a1d2d5d538d8c82ef7e446a2753fbb9
tayek333 <tarekbukhari123@gmail.com>
2022-02-20 18:44:06 -0500

    style: hero mobile responsive

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,12 +5,34 @@
   import porsche from '../images/porsche.png';
   import car1 from '../images/car1.png';
   import car2 from '../images/car2.png';
+  import { media } from './styles/global.styles'
 
   const Hero = styled.section `
 
-  height: 100vh;
-  background: #FBC843;
   padding: 100px 5% 0px;
+  background-color: rgb(251, 200, 67);
+  height: 100vh;
+
+  ${media.m} {
+    position: relative;
+    padding: 10vh 5% 0px;
+    height: unset;
+    display: flex;
+    flex-direction: column;
+    -moz-box-pack: justify;
+    justify-content: space-between;
+  }
+
+  a {
+    color: rgb(65, 74, 105);
+    font-size: 16px;
+    font-weight: bold;
+    letter-spacing: -0.47px;
+    line-height: 54px;
+    text-decoration: none;
+    margin-left: 5px;
+    font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif;
+    }
   `
   const Herotitle = styled.h1`
   color: rgb(255, 255, 255);
@@ -20,6 +42,12 @@
   white-space: pre-line;
   margin: 0px;
   font-weight: normal;
+
+  ${media.m} {
+    font-size: 12.8vw;
+    line-height: 12.8vw;
+  }
+
   `
   const Arrowicon = styled.img`
   position: relative;
@@ -28,6 +56,11 @@
   `
   const Herocontainer = styled.div`
   padding: 50px 0px;
+
+  ${media.m}  {
+    padding: 40px 0px 20vw;
+    
+  }
   `
   const Subcontainer = styled.div`
   color: rgb(65, 74, 105);
@@ -39,33 +72,63 @@
   font-family: 'Eina03';
   `
   const Carscontainer = styled.div`
-
+  ${media.m}  {
+    overflow: hidden;
+    width: 100%;
+    height: 30vh;
+    position: absolute;
+    left: 0px;
+    bottom: -5vh;
+  }
   `
   const Whitespace = styled.div`
   position: absolute;
-  bottom: 0px;
+  bottom: -15px;
   left: 0px;
   width: 100%;
-  height: 200px;
+  height: 215px;
   background-color: rgb(255, 255, 255);
+
+  ${media.m} {
+    display: none
+  }
+
   `
   const Car1 = styled.img`
   position: absolute;
     width: 30vw;
     bottom: 105px;
     left: 0%;
+    
+    ${media.m} {
+    width: 47vw;
+    bottom: 0vh;
+  }
   `
   const Car2 = styled.img`
   position: absolute;
   width: 43.8vw;
   bottom: 135px;
   left: 23%;
+
+  ${media.m}  {
+    width: 50vw;
+    bottom: 3vh;
+    left: 55%;
+  }
   `
   const Porsche = styled.img `
   position: absolute;
   width: 45.7vw;
   bottom: 110px;
   left: 6%;
+
+  ${media.m} {
+    width: 68vw;
+    bottom: 0vh;
+    left: 13%;
+
+  }
   `
   const Heroimgcontainer = styled.div`
   position: absolute;
@@ -78,6 +141,11 @@
   `
   const Heroimg = styled.img`
   width: 100%;
+
+  ${media.m} {
+    opacity: 0;
+  }
+  
   `
 
 
