b58432d2804eadd4d3f7f701cb11f2b801f8776c
tayek333 <tarekbukhari123@gmail.com>
2022-02-23 21:46:23 -0500

    needs to lazy load component instead of timer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-
+import React, { useEffect, useState, Suspense, lazy} from "react";
+import { Switch } from 'react-dom';
 import GlobalStyles from "./components/styles/global.styles";
 import HeroComponent from "./components/Hero.js";
 import Header from './components/Header';
@@ -9,26 +9,47 @@ import Section3 from './components/Section3';
 import Pricing from './components/Pricing';
 import CtaSection from './components/CtaSection';
 import Footer from './components/footer';
-import styled from 'styled-components'
+import styled from 'styled-components';
+import LoaderComponent from './components/loaderScreen';
 
 const Main = styled.main`
 min-height: 100vh;
 width: 100vw;
 overflow: hidden;
 `
+
 function App() {
+
+  const [done, setDone] = useState(undefined);
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log('test');
+      setDone(true);
+    }, 500);
+  }, []);
+
+
   return (
-    <Main>
-      <GlobalStyles />
-      <Header />
-      <HeroComponent />
-      <Section1 />
-      <Section2 />
-      <Section3 /> 
-      <Pricing />
-      <CtaSection />
-      <Footer />
-    </Main>
+    
+      <>
+      {! done ? (
+        
+        <LoaderComponent />
+      ):
+        <Main>
+          <GlobalStyles />
+          <Header />
+          <HeroComponent />
+          <Section1 />
+          <Section2 />
+          <Section3 /> 
+          <Pricing />
+          <CtaSection />
+          <Footer />
+        </Main>
+      }
+    </>
   );
 }
 
